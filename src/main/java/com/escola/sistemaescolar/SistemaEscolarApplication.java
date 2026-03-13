40d2c539a171bb940c2d1ea87ce4763f52170a8a
AssisDev <matheusdonaal@gmail.com>
2026-03-12 23:52:11 -0300

    início projeto cadastro alunos java | spring boot

--- src/main/java/com/escola/sistemaescolar/SistemaEscolarApplication.java
+++ src/main/java/com/escola/sistemaescolar/SistemaEscolarApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaEscolarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaEscolarApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(SistemaEscolarApplication.class, args);
+    }
 
 }
